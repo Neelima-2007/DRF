400d6b4201c8e0748c7fb1602bb407dc873bab45
Neelima <dandeneeli2007@gmail.com>
2025-07-02 20:30:23 +0530

    post,update,delete methods added

--- Demo/demoapp/urls.py
+++ Demo/demoapp/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path,include
-from .views import student_list
+from .views import student_list,create_student
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
     # path('api/',include('demoapp.urls'))
-    path('get/',student_list,name='student_list')
+    path('get/',student_list,name='student_list'),
+    path('create/',create_student,name='create_student')
 ]
--- Demo/demoapp/views.py
+++ Demo/demoapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from rest_framework.response import Response
-from .serializer import StudetSerializer
+from .serializer import StudentSerializer
 from .models import Student
 from rest_framework.decorators import api_view
 
@@ -10,6 +10,40 @@ from rest_framework.decorators import api_view
 @api_view(['GET'])
 def student_list(request):
     studen_data = Student.objects.all()
-    student_lists = StudetSerializer(studen_data,many=True)
+    student_lists = StudentSerializer(studen_data,many=True)
     return Response(student_lists.data)
+
+
+@api_view(['POST'])
+def create_student(request):
+    student_data =StudentSerializer(data=request.data)
+    if student_data.is_valid():
+        student_data.save()
+    return Response(student_data.data)
+
+
+@api_view(['UPDATE'])
+def update_student(request,id):
+    student_data = Student.objects.get(id=id)
+    student_lists = StudentSerializer(instance=student_data,data=request.data)
+    if student_lists.is_valid():
+        student_lists.save()
+    return Response(student_lists.data)
+
+
+@api_view(['DELETE'])
+def delete_student(request,id):
+    student_data = Student.objectts.get(id=id)
+    student_data.delete()
+    return Response('Student Deleted Successfully')
+
+@api_view(['GET'])
+def get_student(request,id):
+    student_data = Student.objects.get(id=id)
+    student_info = StudentSerializer(student_data)
+    return Response(student_info.data)
+
+
+
+
 
